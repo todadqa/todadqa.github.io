5d60d098cbd09b50312af633354dc46dab61d514
Thomas Nguyen <todadqa@todadqa-mbp.home>
2017-04-04 23:12:55 +0100

    Finished website

--- js/namthai.js
+++ js/namthai.js
@@ -1,7 +1,18 @@
 /* javascript/jquery for namthai website */
 
 $( document ).ready(function() {
-      $(".button-collapse").sideNav();
-      $('.parallax').parallax();
-      $('container').height($(window).height()/2);
-    });
+    $(".button-collapse").sideNav();
+	$('.parallax').parallax();
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+	$('container').height($(window).height()/2);
+	$(".menuscrollbutton").click(function() {
+    	$('html,body').animate({
+        scrollTop: $("#menutopscroll").offset().top},
+        'slow');
+	});
+	$(".scrollbutton").click(function() {
+    	$('html,body').animate({
+        scrollTop: $("#mainpart").offset().top},
+        'slow');
+	});
+});
